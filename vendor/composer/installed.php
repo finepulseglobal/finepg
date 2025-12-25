65e6eaccef916a0a1c37c4ac287d7560647f8740
finepulseglobal <finepulsegloballimited@gmail.com>
2025-12-25 03:40:43 -0900

    chore: prepare workspace update - push current changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'finepg/finepg-website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b738dd82bd3b56d713c41657eb68604a1f33abc0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'finepg/finepg-website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b738dd82bd3b56d713c41657eb68604a1f33abc0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
